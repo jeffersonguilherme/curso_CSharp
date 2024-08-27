478742dabb9c682bfa9b2667e3360b7aeb362318
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2024-08-27 07:08:20 -0300

    Aula C#
    Conteúdo das aulas até a aula 17

--- TiposPrimitivos/Program.cs
+++ TiposPrimitivos/Program.cs
@@ -2,8 +2,28 @@
     class Program {
         static void Main(string[] args){
             
-            //int numero inteiro
-          
+            byte meuByte = 127;
+            int idade = 25;
+            float salario = 2.500f;
+            double salarioSemestral = 22.000;// tipo padrão caso coloque var (valor intermediario)
+            decimal salarioAnual = 25.000m;
+
+            bool  usuarioJaCadastrado = false;
+            bool pogamentoRecebido = true;
+            var usuarioExpirado = false; //converte para bool o tipo
+
+        //  char primeiraLetra = ""; //sempre aspas simples
+            char primeiroLetra = 'C';
+            var segundaLetra = 'D';
+
+            string primeiraLetraString = "C"; //Cadeia de caracteres " "
+            string texto = "Meu texto";
+            var documento = "112233311";
+
+            //var substitui o nome de um tipo, sera o primeiro tipo valor atribuido
+            var idadeVar = 25;
+            IEnumerable<MeuTipoComplexo> aluno = new IEnumerable<MeuTipoComplexo>();
+            var aluno = new IEnumerable<MeuTipoComplexo>();
         }
     }
 }
