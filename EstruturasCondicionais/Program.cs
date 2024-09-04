70b09e859d436cadac4120c8ea4e0c8b5a07453a
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2024-09-03 22:02:41 -0300

    Curso C#
    Estrutura condicionais
    Operadores

---
+++ EstruturasCondicionais/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace EstruturasCondicionais {
+    class Program {
+        static void Main(string[] args){
+
+            int idade = 18;
+            int maiorIdade = 21;
+            int idadeMaxima = 65;
+
+        //  if(idade >= maiorIdade && idade < idadeMaxima)
+        //  if(idade >= maiorIdade || idade < idadeMaxima)
+        //  if(idade >= maiorIdade != idade < idadeMaxima)
+        //    if(!(idade >= maiorIdade))
+            if(idade <= maiorIdade)
+                 Console.WriteLine("Sim");
+            else{
+                Console.WriteLine("Não");
+            }
+
+        }
+    }
+}
---
+++ Operadores/Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Operadores{
+    class Program {
+        static void Main(string[] args){
+
+            // -Soma => +
+            // Subtração => -
+            // Multiplicação => *
+            // Divisão => /
+
+
+            // x += 5 é o mesmo que  x = x + 5
+            double x = 0;
+            x += 5; // x = x + 5
+            x -= 1; // x = x - 1
+            x *= 10; // x = x * 10
+            x /= 2;  // x = x / 2
+
+
+            // Operações Lógicas
+
+            // E - end 
+            //      Deve atender TODAS as condições
+            //      &&
+            // OU - or
+            //      Se atender uma condição já retorna verdadeiro
+            //      ||
+            // Negação - not
+            //      !
+        }
+    }
+}
