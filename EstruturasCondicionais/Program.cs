b174feb7c3313e771dc2644b5faa439d9ca91c84
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2024-09-09 21:17:59 -0300

    curso C#

--- EstruturasCondicionais/Program.cs
+++ EstruturasCondicionais/Program.cs
@@ -3,19 +3,49 @@ namespace EstruturasCondicionais {
     class Program {
         static void Main(string[] args){
 
-            int idade = 18;
-            int maiorIdade = 21;
-            int idadeMaxima = 65;
+        //    int idade = 18;
+        //    int maiorIdade = 21;
+        //    int idadeMaxima = 65;
 
         //  if(idade >= maiorIdade && idade < idadeMaxima)
         //  if(idade >= maiorIdade || idade < idadeMaxima)
         //  if(idade >= maiorIdade != idade < idadeMaxima)
         //    if(!(idade >= maiorIdade))
-            if(idade <= maiorIdade)
-                 Console.WriteLine("Sim");
-            else{
-                Console.WriteLine("Não");
-            }
+        //    if(idade <= maiorIdade)
+        //         Console.WriteLine("Sim");
+        //    else{
+        //        Console.WriteLine("Não");
+
+       // int valor = 5;
+       // switch(valor) {
+       //     case 1: Console.WriteLine("1"); break;
+       //     case 2: Console.WriteLine("2"); break;
+       //     case 3: Console.WriteLine("3"); break;
+       //     default: Console.WriteLine("Nem um valor"); break;
+       // }
+
+       //precisamos do contatodor
+
+      // for(var i = 0; i <= 5; i++){
+      //  Console.WriteLine(i);
+      // }
+
+      // for(var i = 5; i >= 0; i--){
+      //  Console.WriteLine(i);
+      // }
+
+      //int valor = 0;
+
+        // while(valor <= 10){
+        //  Console.WriteLine("te amo Renata <3");
+        //   valor++;
+        //  }
+
+        var valor = 0;
+        do {
+         Console.WriteLine("teste");
+         valor++;
+        }while(valor<5);
 
         }
     }
---
+++ FuncoesMetados/Program.cs
@@ -0,0 +1,41 @@
+﻿
+namespace FuncoesMetados{
+    class Program {
+        static void Main(string[] args){
+
+           // MeuMetodo("C# é legal");
+
+           // static void MeuMetodo(string paramentro){
+           //     Console.WriteLine(paramentro);
+           // }
+
+           //static string RetornaNome(string nome, string sobrenome){
+            //Retorna o nome e sobrenome
+          //  return nome + " " + sobrenome;
+         //  }
+
+         //  Console.WriteLine(RetornaNome("jefferson","Silva"));
+        MeuMetodo();
+
+        string nome = RetornaNome("Jefferson ", "silva", 40);
+        Console.WriteLine(nome);
+
+        }
+
+       static void MeuMetodo(){
+        Console.WriteLine("C# é legal");
+       }
+        static string RetornaNome(
+            string nome,
+            string sobrenome,
+            int idade = 30,
+            bool teste = false,
+            double novo = 33.42
+            ){
+            
+            return nome + sobrenome + " tem " + idade.ToString();
+
+        }
+
+    }
+}
