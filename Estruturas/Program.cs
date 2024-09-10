45779ed8bcbbc51ed91a81d03b375485dbfdc085
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2024-09-09 21:57:54 -0300

    Curso C#

---
+++ Estruturas/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Estruturas {
+    class Program {
+        static void Main(string[] args){
+
+            struct Product {
+                //Propriedades
+                public int Id;
+                public float Price;
+
+                //Métodos
+                public float PriceInDolar(float dolar){
+                    return Price * dolar;
+                }
+                
+            }
+
+        }
+    }
+}
