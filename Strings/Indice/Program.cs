7a25572dd55da1fd104887de2a7b2a43f227d6bd
Jefferson Guilherme <81498125+jeffersonguilherme@users.noreply.github.com>
2024-09-14 17:54:03 -0300

    Curso C#

---
+++ Strings/Indice/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Indice{
+    class Program{
+        static void Main(string[] args){
+            var texto = "Este texto é um teste";
+            //Console.WriteLine(texto.IndexOf("é"));
+            //Console.WriteLine(texto.LastIndexOf("S", StringComparison.OrdinalIgnoreCase));
+            //Console.WriteLine(texto.ToLower());
+            //Console.WriteLine(texto.ToUpper());
+            //Console.WriteLine(texto.Insert(texto.Length, "AQUI "));
+            //Console.WriteLine(texto.Remove(5, 5));
+            //Console.WriteLine(texto.Length + 1);
+
+            Console.WriteLine(texto.Replace("e", "X"));
+
+        }
+    }
+}
